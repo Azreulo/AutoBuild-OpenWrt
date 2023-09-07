f302d9b7e4d2b83fffff6e209097c6933e55bd23
Raphael <Azreulo@Gmail.Com>
2023-09-07 14:15:48 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-#=================================================
-# Description: DIY script
-# Lisence: MIT
-# Author: eSirPlayground
-# Youtube Channel: https://goo.gl/fvkdwm 
-#=================================================
-#1. Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
 
-#2. Clear the login password
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
-
-#3. Replace with JerryKuKu’s Argon
-#rm openwrt/package/lean/luci-theme-argon -rf
+sed -i 's|192.168.1.1|10.0.0.100|' package/base-files/files/bin/config_generate #修改默认IP地址
+sed -i 's|+luci-theme-bootstrap ||' feeds/luci/collections/luci/Makefile #删除默认主题
+sed -i 's|+luci-theme-bootstrap ||' feeds/luci/collections/luci-ssl-nginx/Makefile #删除默认主题
+sed -i 's|+luci-theme-bootstrap ||' feeds/luci/collections/luci-light/Makefile #删除默认主题，ImmortalWrt版luci需要
+sed -i 's|+luci ||' feeds/luci/libs/luci-lib-fs/Makefile #删除luci uhttpd关联
+sed -i 's|+aria2||' feeds/packages/net/ariang/Makefile #删除AriaNg与Aria2关联
+sed -i 's|+ariang||' feeds/luci/applications/luci-app-aria2/Makefile #删除AriaNg与Aria2关联
+sed -i 's|http:\/\/\[DOMAIN]:\[PASSWORD]@dyn\.dns\.he\.net\/nic\/update?hostname=\[DOMAIN]\&myip=\[IP]|http:\/\/dyn\.dns\.he\.net\/nic\/update?hostname=\[DOMAIN]\&password=\[PASSWORD]\&myip=\[IP]|g' feeds/packages/net/ddns-scripts/files/services #Lean版Openwrt，修正He.net DDNS脚本错误
+sed -i 's|http:\/\/\[DOMAIN]:\[PASSWORD]@dyn\.dns\.he\.net\/nic\/update?hostname=\[DOMAIN]\&myip=\[IP]|http:\/\/dyn\.dns\.he\.net\/nic\/update?hostname=\[DOMAIN]\&password=\[PASSWORD]\&myip=\[IP]|g' feeds/packages/net/ddns-scripts/files/usr/share/ddns/default/he.net.json #原版OpenWrt，修正He.net DDNS脚本错误，ImmortalWrt版
+sed -i 's|status fastpath|status fastpath >/dev/null|g' feeds/luci/applications/luci-app-turboacc/luasrc/controller/turboacc.lua #修复TurboACC显示不正常
